53cdcaec3691830500de0c7d5b88d875c13a38e7
Rahilkoshti29 <138675780+Rahilkoshti29@users.noreply.github.com>
2025-08-04 15:07:36 +0530

    Marksheet.py

--- Marksheet.py
+++ Marksheet.py
@@ -3,7 +3,7 @@ gr=0
 sub1 = int(input("Enter C Marks:"))
 if(sub1<40):
     f = f+1
-    gr = gr + (40 - sub1)
+    gr = 40 - sub1
 sub2 = int(input("Enter C++ Marks:"))
 if(sub2<40):
     f = f+1
@@ -21,25 +21,26 @@ if(sub5<40):
     f = f+1
     gr = gr + (40 - sub5)
 
-if(f>2):
-    print("You are failed")
-elif(gr<=7):
-    print("You are passed with gracing",gr)
+if(f<=2):
+    if(gr<=7):
+        total = sub1+sub2+sub3+sub4+sub5
+        per = (total/500)*100
+        print("Total Marks is:",total)
+        print("Percentage is:",per)
+        
+        if(per>=84):
+            print("Grade A")
+        elif(per<84 and per>=70):
+            print("Grade B")
+        elif(per<70 and per>=54):
+            print("Grade C")
+        elif(per<54 and per>=40):
+            print("Grade D")
+        else:
+            print("Grade E")
+        if(gr>0):
+            print("You passes with gracing marks",gm)
+    else:
+        print("Your Grace marks is more than seven")
 else:
-    print("You are passed")
-    
-total = sub1+sub2+sub3+sub4+sub5+gr
-per = (total/500)*100
-print("Total Marks is:",total)
-print("Percentage is:",per)
-
-if(per>=84):
-    print("Grade A")
-elif(per<84 and per>=70):
-    print("Grade B")
-elif(per<70 and per>=54):
-    print("Grade C")
-elif(per<54 and per>=40):
-    print("Grade D")
-else:
-    print("You are failed")
+    print("You are failed bcz your failing subject is more than 2")
